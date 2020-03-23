33b64f517b9359c2fb5ce06cc2f2727197709509
Tino Melvan <tmelva00@fesb.hr>
2020-03-23 22:30:09 +0100

    Add core C Random

--- c/Random.c
+++ c/Random.c
@@ -1,1 +1,77 @@
+#include <stdlib.h>
+#include <stdint.h>
+
 #include "Random.h"
+
+#define SRAND_DEFAULT_SEED      (1)
+
+static int32_t __Next(void);
+static int32_t __NextBound(int32_t floor, int32_t ceiling);
+
+static uint64_t Rdtsc(void);
+
+static uint64_t GenerateSeed(enum TypeofSeed seedType);
+
+static Random __generator = {
+        .initialized = SEED_UNINITIALIZED,
+        .seed = SRAND_DEFAULT_SEED,
+
+        .Next = &__Next,
+        .NextBound = &__NextBound
+};
+
+static int32_t __Next(void)
+{
+        return rand();
+}
+
+static int32_t __NextBound(int32_t floor, int32_t ceiling)
+{
+        return (rand() % (ceiling - floor + 1)) + floor;
+}
+
+static uint64_t Rdtsc(void)
+{
+        uint32_t lo;
+        uint32_t hi;
+
+        __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
+
+        return ((uint64_t)hi << 32) | lo;
+}
+
+static uint64_t GenerateSeed(enum TypeofSeed seedType)
+{
+        if (seedType == SEED_KEEP_USED ||
+            seedType == __generator.initialized) {
+                return __generator.seed;
+        }
+
+        if (seedType == FORCE_RESEED) {
+                seedType = __generator.initialized;
+        }
+
+        switch (seedType) {
+        case SEED_RDTSC:
+                return Rdtsc();
+        case SEED_UNINITIALIZED:
+        default:
+                return SRAND_DEFAULT_SEED;
+        }
+}
+
+Random RandomGenerator(enum TypeofSeed seedType)
+{
+        uint64_t seed = GenerateSeed(seedType);
+        if (seed != __generator.seed) {
+                __generator.seed = seed;
+                srand(seed);
+        }
+
+        if (seedType != SEED_KEEP_USED ||
+            seedType != FORCE_RESEED) {
+                __generator.initialized = seedType;
+        }
+
+        return __generator;
+}
--- c/Random.h
+++ c/Random.h
@@ -1,8 +1,23 @@
 #ifndef C_RANDOM_H
 #define C_RANDOM_H
 
-/*
- * TODO
- */
+#include <stdint.h>
+
+enum TypeofSeed {
+        SEED_UNINITIALIZED,
+        SEED_RDTSC,
+        SEED_KEEP_USED,
+        FORCE_RESEED
+};
+
+typedef struct Random {
+        enum TypeofSeed initialized;
+        uint64_t seed;
+
+        int32_t (*Next)(void);
+        int32_t (*NextBound)(int32_t floor, int32_t ceiling);
+} Random;
+
+Random RandomGenerator(enum TypeofSeed seedType);
 
 #endif  // !C_RANDOM_H
